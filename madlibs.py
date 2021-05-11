0dff7666e13ba82975caed8f4d0988ab49264443
abercilla <annebercilla@gmail.com>
2021-05-11 21:08:07 +0000

    added goodbye.html

---
+++ madlibs.py
@@ -0,0 +1,67 @@
+"""A madlib game that compliments its users."""
+
+from random import choice
+
+from flask import Flask, render_template, request
+
+# "__name__" is a special Python variable for the name of the current module.
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful',
+    'smashing', 'lovely',
+]
+
+
+@app.route('/')
+def start_here():
+    """Display homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route('/greet')
+def greet_person():
+    """Greet user with compliment."""
+
+    player = request.args.get("person")
+
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html",
+                           person=player,
+                           compliment=compliment)
+
+
+@app.route('/game')
+def show_madlib_form():
+    """Input whther user wants to play or not"""
+
+    #If name/game = value/yes then render_template game.html
+    #If name/game = value/no then render_template goodbye.html
+
+    #make sure name  matches the radio button in compliments form
+    willplay = request.args.get("willplay")
+    print(f"willplay = {willplay}")
+
+    # if willplay == "no":
+    #     return render_template("goodbye.html", value = "no")
+    # else:
+    #     return render_template("game.html")
+
+
+
+if __name__ == '__main__':
+    # Setting debug=True gives us error messages in the browser and also
+    # "reloads" our web app if we change the code.
+
+    app.run(debug=True, host="0.0.0.0")
